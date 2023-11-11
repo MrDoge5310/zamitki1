9788be7699e0bcf2c6461e9e841a57bd51f086b1
LOGIKA <b56642694@gmail.com>
2023-11-11 12:33:11 +0200

    added note showing

--- main.py
+++ main.py
@@ -7,6 +7,27 @@ def WriteToFile(data):
         json.dump(data, file)
 
 
+def showNote():
+    key = notes_list.selectedItems()[0].text()
+    text_field.setText(notes[key]["текст"])
+    tag_list.clear()
+    tag_list.addItems(notes[key]["теги"])
+
+
+def createNote():
+    name, ok = QInputDialog.getText(window, "Додати нотатку", "Назва нотатки:")
+    if ok:
+        if name != "":
+            notes[name] = {"текст": "", "теги": []}
+            notes_list.addItem(name)
+        else:
+            QMessageBox.warning(window, "Помилка", "Невірно вказана назва")
+
+
+notes_list.itemClicked.connect(showNote)
+create_note.clicked.connect(createNote)
+
+
 with open("BIBI.json", 'r') as file:
     notes = json.load(file)
 
