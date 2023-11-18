60c7d87267248b76ba598a9b7db2d4abe86c95a7
LOGIKA <b56642694@gmail.com>
2023-11-18 11:51:44 +0200

    added note showing

--- main.py
+++ main.py
@@ -24,8 +24,31 @@ def createNote():
             QMessageBox.warning(window, "Помилка", "Невірно вказана назва")
 
 
+def deleteNote():
+    key = notes_list.selectedItems()[0].text()
+    del notes[key]
+    notes_list.clear()
+    tag_list.clear()
+    text_field.clear()
+    notes_list.addItems(notes)
+    WriteToFile(notes)
+
+
+def saveNote():
+    if notes_list.selectedItems():
+        key = notes_list.selectedItems()[0].text()
+        text = text_field.toPlainText()
+        notes[key]["текст"] = text
+        WriteToFile(notes)
+        QMessageBox.information(notes_win, "Успішно", "Замітку збережено")
+    else:
+        QMessageBox.warning(notes_win, "Помилка", "Оберіть замітку")
+
+
 notes_list.itemClicked.connect(showNote)
 create_note.clicked.connect(createNote)
+delete_note.clicked.connect(deleteNote)
+save_note.clicked.connect(saveNote)
 
 
 with open("BIBI.json", 'r') as file:
